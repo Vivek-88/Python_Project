269eaf527e8fc042175cec18160752f9225a4d11
Vivek Kumar <vivek.kumar@consultadd.com>
2024-01-12 14:20:14 +0530

    operaters use

---
+++ operaters.py
@@ -0,0 +1,11 @@
+num1 =7
+num2 =3
+
+# Arithmetic Operation
+
+print("num1 + num2 is ", num1+num2)
+print("num1 - num2 is ", num1-num2)
+print("num1 * num2 is ", num1*num2)
+print("num1 / num2 is ", num1/num2)
+print("num1 // num2 is ", num1//num2) # perfect division
+print("num1 ** num2 is ", num1**num2) # a^b Power
