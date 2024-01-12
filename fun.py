4eac6f6784b363a5f48dca74235c347ec5bf8032
Vivek Kumar <vivek.kumar@consultadd.com>
2024-01-12 16:36:24 +0530

    function

---
+++ fun.py
@@ -0,0 +1,8 @@
+def greetHello(name,age) :
+    print("function start")
+    print("Hello "+name,"Your age is",age)
+    print("function end")
+
+greetHello("Vivek",10)
+# greetHello()
+# greetHello()
