f304d7244f2091464ee271c52131a61c9c746a06
Vivek Kumar <vivek.kumar@consultadd.com>
2024-01-12 16:31:04 +0530

    learnt loop

---
+++ loop.py
@@ -0,0 +1,26 @@
+for i in range(0,3) :
+    print(i)
+
+for i in range(5) :
+    print(i)
+
+for i in range(4,8) :
+    print(i)
+
+list = [3,6,2,5,8,9,5]
+
+print("Printing list")
+for item in list :
+    print(item)
+
+set = {2,6,3,5,7,8,4,5,6,4,4,6}
+
+print("Set printing")
+for s in set :
+    print(s)
+
+
+i=0
+while(i<10) :
+    print(i)
+    i+=1
