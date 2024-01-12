d7007c4dcabe14ae38c19e840a1fda329b63f4dd
Vivek Kumar <vivek.kumar@consultadd.com>
2024-01-12 09:21:26 +0530

    learn comment

---
+++ comment.py
@@ -0,0 +1,16 @@
+print("hello World 1")
+
+# Single Line Comment
+# print("hello World 2")
+
+print("hello World 3")
+
+# multi line comment
+'''
+print("hello World 4")
+
+print("hello World 5")
+
+'''
+
+print("hello World 6")
